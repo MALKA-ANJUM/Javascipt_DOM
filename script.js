5d1e6401507c404f605d2c46c370c955a67f7f11
Malka <malkaanjum1999@gmail.com>
2022-07-21 12:32:15 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+let btn = document.querySelector('button')
+btn.addEventListener('click', buton = () => {
+   let ipt = prompt("say hello")
+   btn.innerText = ipt + " malka"
+});
+
+// async -->
